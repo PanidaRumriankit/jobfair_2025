d8ae9bb6f279890a67daf370c0cb2366e8cc8763
PanidaRumriankit <panida.rum@ku.th>
2025-01-09 00:03:56 +0700

    feat: add to be developed theme feature and suppress hydration warning

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from "next-themes"
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,11 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
