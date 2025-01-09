553bbb2d98ded845078fbe7a03efc3a18fdadb86
PanidaRumriankit <panida.rum@ku.th>
2025-01-09 22:28:17 +0700

    feat: add logo image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import LoginForm from "./components/Loginform";
+import LoginForm from "./components/LoginForm";
 import white_logo from "../../public/white_logo.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
-      <Image src={white_logo} alt="white_logo" width="228" height="159" />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black">
+      <Image src={white_logo} alt="KU Job Fair logo" width="228" height="159" />
       <LoginForm />
     </div>
   );
