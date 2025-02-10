40a7353edf838806497fed0296e0cde42fb0b644
PanidaRumriankit <panida.rum@ku.th>
2025-02-11 02:05:31 +0700

    feat: error popup new design

--- frontend/src/app/components/ErrorPopup.tsx
+++ frontend/src/app/components/ErrorPopup.tsx
@@ -1,34 +1,40 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import cross from '../../../public/cross.png'
+import Image from 'next/image';
+import cross from '../../../public/cross.png';
 import {
   Dialog,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import { useTranslation } from 'react-i18next';
 import "../../../i18n";
 
-export default function ErrorPopup() {
+interface ErrorPopupProps {
+  message: string;
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+export default function ErrorPopup({ message, onClose, isOpen }: ErrorPopupProps) {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <Dialog>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle className="flex items-center gap-2 text-destructive">
-              {t('error')}
-            </DialogTitle>
-          </DialogHeader>
-          <DialogFooter>
-            <Image src={cross} alt="Error" />
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  )
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          
+          <DialogTitle className="flex flex-col items-center justify-center gap-2 text-destructive">
+          <Image src={cross} alt="Error" width={50} height={50} />
+            {t('error')}
+          </DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-center text-destructive">{message}</p>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 }
