2779bde2db681588693200ed93d567f291381153
PanidaRumriankit <panida.rum@ku.th>
2025-02-11 02:07:17 +0700

    feat: session timeout popup new design

--- frontend/src/app/components/SessionTimeout.tsx
+++ frontend/src/app/components/SessionTimeout.tsx
@@ -7,29 +7,34 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import { useRouter } from 'next/navigation';
 import { useTranslation } from 'react-i18next';
 import "../../../i18n";
 
-export function ErrorPopup() {
+interface ErrorPopupProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function SessionTimeout({ isOpen, onClose }: ErrorPopupProps) {
   const { t } = useTranslation();
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <Dialog>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle className="flex items-center gap-2 text-destructive">
-              {t('timeout')}
-            </DialogTitle>
-          </DialogHeader>
-          <DialogFooter>
-            <Button onClick={() => router.push('/')}>{t('return')}</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  )
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px] flex flex-col justify-center items-center [&>button]:hidden">
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center gap-2 text-destructive">
+            {t('timeout')}
+          </DialogTitle>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={() => { router.push('/'); onClose(); }}>
+            {t('return')}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
