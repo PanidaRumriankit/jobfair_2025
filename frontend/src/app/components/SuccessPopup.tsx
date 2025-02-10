6eceec2e6154c4f71c8346442fe29694f7067d4f
PanidaRumriankit <panida.rum@ku.th>
2025-02-11 02:07:34 +0700

    feat: success popup new design

--- frontend/src/app/components/SuccessPopup.tsx
+++ frontend/src/app/components/SuccessPopup.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import checked from '../../../public/checked.png'
+import Image from "next/image";
+import checked from "../../../public/checked.png";
 import {
   Dialog,
   DialogContent,
@@ -9,30 +9,34 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
-import { useTranslation } from 'react-i18next';
+} from "@/components/ui/dialog";
+import { useTranslation } from "react-i18next";
 import "../../../i18n";
 
-export default function ErrorPopup() {
+interface SuccessPopupProps {
+  isOpen: boolean;
+  ssid?: string;
+  onClose: () => void;
+}
+
+export default function SuccessPopup({ isOpen, ssid, onClose }: SuccessPopupProps) {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <Dialog>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle className="flex items-center gap-2 text-destructive">
-              {t('success')}
-            </DialogTitle>
-          </DialogHeader>
-          <DialogDescription>
-            SSID: 123456789
-          </DialogDescription>
-          <DialogFooter>
-            <Image src={checked} alt="Success" />
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  )
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center gap-2 text-green-600">
+            {t("success")}
+          </DialogTitle>
+        </DialogHeader>
+        <DialogDescription className="flex items-center justify-center">
+          SSID: {ssid ?? "N/A"}
+        </DialogDescription>
+        <DialogFooter>
+          <Image src={checked} alt="Success" className="w-16 h-16 mx-auto" />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
