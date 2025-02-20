417f87070ae550a31d63aa6356d4599b1be18972
PanidaRumriankit <panida.rum@ku.th>
2025-02-21 03:37:01 +0700

    feat: GET /profile

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User extends Document {
+
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
