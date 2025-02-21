317fe21c86c8758b8eaf2854d2cabc29366ee4e0
PanidaRumriankit <panida.rum@ku.th>
2025-02-21 20:42:42 +0700

    feat: enable cors for port 3001

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.enableCors({ origin: "http://localhost:3001", credentials: true });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
