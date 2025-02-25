1619896de8fdba047a2c0f00d149225c60ba9d0d
Panida Rumriankit <146973594+PanidaRumriankit@users.noreply.github.com>
2025-02-26 04:22:58 +0700

    Add files via upload

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Post, UseGuards, Request, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req, @Res({ passthrough: true }) res: Response) {
+    const { access_token } = await this.authService.login(req.user);
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+    });
+    return { message: 'Successfully logged in' };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token');
+    return { message: 'Successfully logged out' };
+  }
+}
